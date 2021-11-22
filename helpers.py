cabd017e46f336eb62a4eb44e87f38128c104215
Fabio Speroni <fabio.speroni@gmail.com>
2021-11-21 17:10:08 -0700

    completed beer recipe search form

--- helpers.py
+++ helpers.py
@@ -4,8 +4,6 @@ def add_image(beers):
     """Iterate through beers and add generic image if needed."""
 
     for beer in beers:
-        print("**************************")
-        print(beer['image_url'])
         if not beer['image_url']:
             beer.update({'image_url': "/static/generic_beer.png"})
     
--- static/app.js
+++ static/app.js
@@ -1,14 +1,61 @@
-//Get the button:
-const mybutton = document.getElementById("myBtn")
+/** Toggle Beer search button enabled/disabled. */
+const checkBeerInput = (e) => {
+  $('#beerBtn').prop('disabled', (e) => {
+    return $("#beerInput").val().trim() === ''
+  })
+}
+$(document).on('change', '#beerInput', checkBeerInput);
 
-window.onscroll = function() {toggleShowButton()}
+/** Toggle Food search button enabled/disabled */
+const checkFoodInput = (e) => {
+  $('#foodBtn').prop('disabled', (e) => {
+    return $("#foodInput").val().trim() === ''
+  })
+}
+$(document).on('change', '#foodInput', checkFoodInput);
+
+/** Toggle Recipe search button enabled/disabled */
+const checkRecipeInput = (e) => {
+  const inputIds = $('.recipe')
+  let isDisabled = true
+  inputIds.each(function() {
+    if ($(this).val().trim() !== '') {
+      isDisabled = false
+      console.log(isDisabled)
+      console.log($(this).val().trim())
+      return false
+    }
+  })
+  $('#recipeBtn').prop('disabled', isDisabled)
+}
+$('#recipeForm').on('change', checkRecipeInput)
+
+/** Initialize tooltips */
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+/** Clear the recipe search form */
+const clearForm = (e) => {
+  e.preventDefault()
+  $('#recipeForm').trigger('reset')
+  $('#recipeBtn').prop('disabled', true)
+}
+$('#clearBtn').on('click', clearForm)
+
+
+//Get the button:
+const scrollButton = document.getElementById("scrollBtn")
+
+// window.onscroll = function() {toggleShowButton()}
 
 /** Shows the button only when user scrolls down 20px from the top of the document*/
 function toggleShowButton() {
   if (document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block"
+    scrollButton.style.display = "block"
   } else {
-    mybutton.style.display = "none"
+    scrollButton.style.display = "none"
   }
 }
 
