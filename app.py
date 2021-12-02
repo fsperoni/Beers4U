ebc7bbd963795c2a6567134bc7ba4436067a6961
Fabio Speroni <fabio.speroni@gmail.com>
2021-12-02 10:10:03 -0700

    updated app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///beers4u')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'temporary_development_key')
-# Delete below before deploying
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SQLALCHEMY_ECHO'] = True
 
